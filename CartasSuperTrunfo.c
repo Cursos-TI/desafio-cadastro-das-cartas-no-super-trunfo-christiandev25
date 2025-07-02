8b87bd3fecd55cdd7903d76d810ddf2eae640cb8
christiandev25 <matheuschristian675@gmail.com>
2025-07-02 19:41:33 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 
-
-
 int main() {
-    
+    int pop1;
+    float area1;
+    float pib1;
+    int pontos1;
+
+    int pop2;
+    float area2;
+    float pib2;
+    int pontos2;
+
+    // Carta 1
+    printf("Escreva a população da carta 1: ");
+    scanf("%d", &pop1);
+
+    printf("Escreva a área da carta 1: ");
+    scanf("%f", &area1);
+
+    printf("Escreva o PIB da carta 1: ");
+    scanf("%f", &pib1);
+
+    printf("Escreva os pontos turísticos da carta 1: ");
+    scanf("%d", &pontos1);
+
+    // Carta 2
+    printf("Escreva a população da carta 2: ");
+    scanf("%d", &pop2);
+
+    printf("Escreva a área da carta 2: ");
+    scanf("%f", &area2);
+
+    printf("Escreva o PIB da carta 2: ");
+    scanf("%f", &pib2);
+
+    printf("Escreva os pontos turísticos da carta 2: ");
+    scanf("%d", &pontos2);
 
     return 0;
 }
